699aff5733ef59e932a26e61cb1d21c5f2614018
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-03-08 14:40:16 +0000

    tests passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(number)
   if number%5 == 0 && number%3 == 0
     'fizzbuzz'
-  end 
+  elsif number%3 == 0
+    'fizz'
+  elsif number%5 == 0
+    'buzz'
+  else
+    number 
+  end
 end
